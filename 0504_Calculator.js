77608045529ae8338ea082cb5afb2eb4e3aaed17
Tahjay <tahjay.watson@gmail.com>
2026-05-07 17:56:32 -0400

    REVAMPED ENTIRE PROJECT
    Changed structure of index to better reflect
    a calculator's physical structure
    
    Nested flexbox containers for better control
    of event propogation
    
    Changed logic using scripts

--- 0504_Calculator.js
+++ 0504_Calculator.js
@@ -1,63 +1,50 @@
 //Tahjay Watson
 //0504_Calculator JavaScript file
 
-//First on the to do list is to create event listeners for each button.
-//Event propagation is likely the best way to go forward
-//I'll be referencing the RPS_Script.js & etch-a-sketch.js files for this as they have a similar setup with the buttons and divs.
-
-//The flow should be
-//1. User clicks a button
-//2. Button click is registered and the value of the button is stored in a variable
-//3. If the value of the button is a number, display it
-//Just for debug to see if we can get this working. We'll add additional logic from there.
-
-let numberContainer = document.querySelector(".numberContainer");
-let displayDigit = document.querySelector(".displayDigit");
-let operandContainer = document.querySelector(".operandContainer");
-let currentOperand = "";
-var currentValue = 0;
-var previousValue = 0;
+const displayBox = document.querySelector(".upperDisplay")
 
 
-let calcArray = [0, 0, 0];
+//UpdateVar
+//Create the functions that update one of your number variables when the calculator’s digit buttons are clicked. 
+// Your calculator’s display should also update to reflect the value of that number variable.
+//>Start Value
+//Number Pressed
+//Previous num is added as the last digit 
 
-//First, select buttons woop woop
-//I want to turn lines  18-23 into a function to be reused.
-function makeEventListener(aParent, aChild) {
-    aParent.addEventListener("click", (event) => {
-        let target = event.target;
-        aChild.textContent = target.textContent;
+var displayValueOne = 0;
 
-    });
+const calcButtons = document.querySelector(".case");
+
+calcButtons.addEventListener("click", (event) => {
+    let target = event.target;
+    updateDisplay(target);
+});
+
+function updateDisplay(pressedButton) {
+    if (pressedButton.closest(".bottomRowLeft")) {
+        displayBox.textContent = displayBox.textContent + pressedButton.textContent;
+    }else if(pressedButton.closest(".topRow")){
+       let storedOperator = chooseOperator(pressedButton.id);
+       alert(storedOperator);
+        
+    }
 }
 
-//[nnn]
-//[ooo]
-//non
-//ono
-//oon
-//noo
-//
 
+function chooseOperator(operator) {
+    switch (operator) { //evaluate based on id
 
-
-makeEventListener(numberContainer, displayDigit);
-makeEventListener(operandContainer, currentOperand);
-
-//Calculators let you override previous numbers if you hit one number
-//To emulate that we're going to use a "currentValue" variable that will be overridden if the user
-//presses another number button instead of an operand.
-//I'm thinking the workflow should be like this:
-
-
-//1. Current value is assigned 0 at start
-//2. We check input every time a button is pressed. 
-//2.A if the button is a number, override "currentValue" with the value of the button. 
-//2.B if the button is an operand, apply no changes to the "currentValue", store the operand in "currentOperand"
-//3.If there is a value within "currentValue" and "currentOperand", the next number should  change the display to the result
-
-
-//What does this look like? Am I missing something? Probably but we'll come back to it
+        case "plus":
+           return "plus";
+        case "minus":
+          return "minus";
+        case "multiply":
+            return "multiply"
+        case "divide":
+            return "divide";
+            break;
+    }
+}
 
 function add(x, y) {
     return x + y;
